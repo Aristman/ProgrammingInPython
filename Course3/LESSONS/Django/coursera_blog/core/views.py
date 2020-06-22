a685c4752678715a1d1ccffbebcf05593e4a0a3e
Aristman <armobila@yandex.ru>
2020-06-22 19:38:12 +0300

    Третья неделя третьего курса Django

--- Course3/LESSONS/Django/coursera_blog/core/views.py
+++ Course3/LESSONS/Django/coursera_blog/core/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+
 def index(request):
     return HttpResponse('OK')
+
+def my_view(request):
+    t = loader.get_template('templates/core.html')
+    context = {'Ключ': 'Первый'}
+    return HttpResponse(t.render(context, request))

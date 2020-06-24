48c2a90eea126982d96b5d3f647c84ea499c9033
Aristman <armobila@yandex.ru>
2020-06-24 08:09:01 +0300

    Третья неделя третьего курса Django

--- Course3/TESTS/test_routing/routing/template/templatetags/extras.py
+++ Course3/TESTS/test_routing/routing/template/templatetags/extras.py
@@ -2,6 +2,20 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def inc(a, b):
-    return a + b
+    try:
+        return str(int(a) + int(b))
+    except ValueError:
+        return None
+
+
+@register.simple_tag
+def division(a, b, to_int=False):
+    try:
+        a = int(a)
+        b = int(b)
+        return str(int(a / b)) if to_int == True else str(a / b)
+    except Exception:
+        return None
